8da0e5fe3cb59034b4af442699b1f4d9c72e090c
Keith Hamm <hkhamm@gmail.com>
2015-11-09 08:42:18 -0800

    usr, pw, bd, port to config

--- CONFIG.py
+++ CONFIG.py
@@ -3,20 +3,20 @@ Configuration of 'memos' Flask app.
 Edit to fit development or deployment environment.
 
 """
-import random
+# import random
 
 # localhost
 # PORT = 5000
 # DEBUG = True
-# MONGO_PORT = 
+# MONGO_PORT = 27017
 
 # ix.cs.uoregon.edu
-PORT = random.randint(5000, 8000)
+PORT = 7420  # random.randint(5000, 8000)
+MONGO_PORT = 4152
 DEBUG = False  # Because it's unsafe to run outside localhost
 
 # both
-MONGO_PW = ''
-MONGO_USER = ''
-MONGO_PORT = 
-MONGO_DB = ''
+MONGO_USER = 'memos_user'
+MONGO_PW = 'peach-cobbler'
+MONGO_DB = 'memos'
 MONGO_URL = 'mongodb://{$MONGO_USER}:{$MONGO_PW}@localhost:{$MONGO_PORT}/{$MONGO_DB}'
